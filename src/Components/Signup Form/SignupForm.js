276394cbcb15d49a7aa97bfed53926a63addc4ac
sajjad77 <sajjadmashhadi7@gmail.com>
2019-10-25 16:16:45 +0330

    add test hook for username in signup

--- src/Components/Signup Form/SignupForm.js
+++ src/Components/Signup Form/SignupForm.js
@@ -2,11 +2,23 @@ import React, { useState , useEffect} from 'react'
 import style from './SignupForm.module.css'
 
 const SignupForm = (props) => {
-    
+    const [username, setUsername] = useState('')
+    const [error, setError] = useState(null)
+    const handleUsernameChange = (e) => {
+        e.persist()
+        const username = e.target.value
+        if(username.length < 5 ){
+            setError('لطفا بیشتر از 5 کاراکتر وارد کنید')
+        }else{
+            setError(null)
+        }
+            setUsername(username)
+    }
     return (
         <div>
             <form className={style.signupForm}>
-                <label>نام کاربری <input/></label>
+                {error && <p>{error}</p>}
+                <label>نام کاربری <input value={username} onChange={handleUsernameChange} /></label>
                 <label>رمز عبور <input/></label>
                 <label>شماره تلفن <input/></label>
                 <label>شماره دانشجویی <input/></label>
