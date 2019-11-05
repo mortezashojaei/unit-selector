37bee08f7ec463c5d45d2deca3f97bf600e7731f
alefseen <sooltaniyan@gmail.com>
2019-11-05 08:41:14 +0330

    add states for form

--- src/Components/login-form/loginForm.js
+++ src/Components/login-form/loginForm.js
@@ -1,15 +1,39 @@
 import React, { useState , useEffect} from 'react'
-import style from './style.scss';
+import style from './style.scss'
 
-const loginForm= (props)=> {
 
-    return (
+const LoginForm = (props) => {
+    const [username, setUsername]=useState(null)
+    const [password,setPassword]=useState(null)
+    const [error,setError]=useState(null);
+    const handlePasswordChange = (e) => {
+        e.persist()
+        const password = e.target.value 
+        if(password.length < 4){
+            setError('برای رمز عبور بیشتر از 4 کاراکتر وارد کنید')
+        }else{
+            setError(null)
+        }
+        setPassword(password)
+    }
+    const handleUsernameChange = (e) => {
+        e.persist()
+        const username = e.target.value
+        if(username.length < 5 ){
+            setError(' برای نام کاربری بیشتر از 5 کاراکتر وارد کنید')
+        }else{
+            setError(null)
+        }
+            setUsername(username)
+    }
+
+        return (
        <form>
-           <input id="username" placeholder="input username..."></input>
-           <input id="password" placeholder="input password..."></input>
+           <input id="usernme" value={username} placeholder="input username..." onChange={(e)=>{setUsername(e.target.value)}}></input>
+           <input id="password" value={password} placeholder="input password..." onChange={(e)=>{setPassword(e.target.value)}}></input>
            <button type="submit">submit</button>
        </form>
         );
 
   }
-  export default loginForm;
+  export default LoginForm
