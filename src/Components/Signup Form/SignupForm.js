4ae258878c80e7b943c0f1de59c24b8a7c428c4d
sajjad77 <sajjadmashhadi7@gmail.com>
2019-11-08 23:59:06 +0330

    add select-search and error handlers for requests

--- src/Components/Signup Form/SignupForm.js
+++ src/Components/Signup Form/SignupForm.js
@@ -1,5 +1,7 @@
 import React, { useState , useEffect} from 'react'
 import axios from 'axios'
+import SelectSearch from 'react-select-search'
+import Select from 'react-select';
 import style from './SignupForm.module.css'
 
 const SignupForm = (props) => {
@@ -11,8 +13,18 @@ const SignupForm = (props) => {
     const [fullName, setFullName] = useState()
     const [error, setError] = useState(null)
 
+    const toSelectForm = (majors) => {
+        const majorsCopy = []
+         majors.map(major => {
+                console.log(majorsCopy)
+                return majorsCopy.push({label: major.name, value: major.id})
+            })
+            return majorsCopy
+    }
+
     useEffect(() => {
         axios.get('http://localhost:3000/majors').then(response => {
+            //change response.data to response.data.data from radin code
             setMajors(response.data)
         }).catch(err => console.log(err))
     },[])
@@ -20,7 +32,7 @@ const SignupForm = (props) => {
     const handleUsernameChange = (e) => {
         e.persist()
         const username = e.target.value
-        if(username.length < 5 ){
+        if(username.length < 2 ){
             setError(' برای نام کاربری بیشتر از 5 کاراکتر وارد کنید')
         }else{
             setError(null)
@@ -48,26 +60,38 @@ const SignupForm = (props) => {
         setSemester(term)
     }
     const handleFullNameChange = (e) => {
+        e.persist()
         const name = e.target.value 
         setFullName(name)
     }
     const handleMajorChange = (e) => {
-        const major = e.target.value
+        const major = e.value
         setMajor(major)
+        console.log(e)
     }
     
     const onFormSubmit = (e) => {
         e.preventDefault()
         if(!error){
+            console.log(major)
             axios.post('http://localhost:3000/users',{
                 username,
                 password,
                 semester,
                 major,
                 full_name: fullName
-            }).then(() => {
-                console.log('done')
-                alert('done')
+            }).then((res) => {
+                let status = res.status
+                if(status === 200 || status === 201){
+                    console.log(res)
+                    alert('done')
+                }else if(status === 409){
+                    setError('حساب کاربری با این نام کاربری موجود است')
+                }else if(status === 406){
+                    setError('خطا در سرور ! لطفا بعدا اقدام کنید')
+                }else if(status === 404 || status === 400){
+                    setError('رشته وارد شده صحیح نمیباشد')
+                }
             }).catch(err => console.log(err))
         }else{
             alert('error')
@@ -98,9 +122,12 @@ const SignupForm = (props) => {
                 />
                 </label>
                 <label>رشته
-                <select onChange={handleMajorChange}>
-                    {majors.map(major => <option value={major.id}>{major.name}</option>)}
-                </select>
+                <Select
+                className={style.selectSearch}
+                placeholder="انتخاب کنید"
+                options={toSelectForm(majors)}
+                onChange={handleMajorChange}
+                />
                 </label>
                 <label>نام و نام خانوادگی
                 <input
