0d36db72cc3b204272fe8c10138b01481301e7df
sinamoraddar <sinamoraddar@gmail.com>
2019-11-29 17:02:56 +0330

    fix a typo

--- src/Components/App/Routing.js
+++ src/Components/App/Routing.js
@@ -2,14 +2,14 @@ import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Welcome from "../Welcome/Welcome";
 import SignupForm from "Components/Signup Form/SignupForm";
-import loginForm from "Components/login-form/LoginForm";
+import LoginForm from "Components/login-form/LoginForm";
 function App() {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={Welcome} />
         <Route path="/signup" exact component={SignupForm} />
-        <Route path="/login" exact component={loginForm} />
+        <Route path="/login" exact component={LoginForm} />
       </Switch>
     </BrowserRouter>
   );
