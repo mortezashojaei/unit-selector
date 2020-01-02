74953baefb90240e9a878d1f00cc4062d61cfc97
sinamoraddar <sinamoraddar@gmail.com>
2020-01-02 12:41:07 +0330

    fix some issues with the login page's styles

--- src/Components/Welcome/Welcome.js
+++ src/Components/Welcome/Welcome.js
@@ -1,26 +1,32 @@
 import React from "react";
 import { Link, Route } from "react-router-dom";
 import SignupForm from "Components/SignupForm/SignupForm";
-import SignupContainer from '../../Components/SignupContainer/SignupContainer'
+import SignupContainer from "../../Components/SignupContainer/SignupContainer";
 import LoginForm from "Components/LoginForm/LoginForm";
 import styles from "./Welcome.module.scss";
 import TabBar from "Components/TabBar/TabBar";
 
 const Welcome = () => (
   <>
-    <header>
-      <TabBar />
-    </header>
     <main className={`${styles.welcome} container`}>
-      <figure>
-        <img src="./assets/images/khuLogo-black.png" alt="دانشگاه خوارزمی" />
-        <figcaption>
-          <h1>سیستم شبیه ساز انتخاب واحد</h1>
-          برای دانشجویان : دانشگاه خوارزمی
-        </figcaption>
-      </figure>
-      <Route path="/signup" exact component={SignupContainer} />
-      <Route path="/login" exact component={LoginForm} />
+      <section>
+        <figure>
+          <div className={styles.logo}>logo will be here</div>
+          <figcaption>
+            <h1>سیستم شبیه ساز انتخاب واحد</h1>
+            <div>
+              برای دانشجویان : دانشگاه خوارزمی
+              <img
+                src="./assets/images/khuLogo-black.png"
+                alt="دانشگاه خوارزمی"
+              />
+            </div>
+          </figcaption>
+        </figure>
+        <TabBar />
+        <Route path="/signup" exact component={SignupContainer} />
+        <Route path="/login" exact component={LoginForm} />
+      </section>
     </main>
   </>
 );
