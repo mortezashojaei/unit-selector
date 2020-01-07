88223537330009c50cb43299bead8413e4f19884
sinamoraddar <sinamoraddar@gmail.com>
2020-01-07 22:42:16 +0330

    add some basic styles to the navbar

--- src/Components/Dashboard/NavBar/NavBar.js
+++ src/Components/Dashboard/NavBar/NavBar.js
@@ -1,23 +1,42 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useCallback, useState } from "react";
+import { Link, Redirect, withRouter } from "react-router-dom";
 import styles from "./NavBar.module.scss";
 
-const NavBar = () => {
+const NavBar = ({ history }) => {
+  const [isMenuClosed, setIsMenuClosed] = useState(true);
+  const logout = useCallback(() => {
+    history.push("/");
+  }, [history]);
+
   return (
     <nav className={styles.navBar}>
       <div className={styles.logo}>لوگو</div>
       <div className={styles.buttonContainer}>
-        <button>خروج</button>
-        <button>ویرایش اطلاعات</button>
+        <button onClick={logout}>
+          خروج <span className={styles.circle}></span>
+        </button>
+        <button onClick={() => alert("blah blah blah")}>
+          ویرایش اطلاعات <span className={styles.circle}></span>
+        </button>
       </div>
-      <div className={styles.linkContainer}>
+      <div
+        className={`${styles.linkContainer} ${
+          isMenuClosed ? styles.closed : null
+        }`}
+      >
         <Link to="#">لینک اول منو</Link>
         <Link to="#">لینک دوم منو</Link>
         <Link to="#">لینک سوم منو</Link>
         <Link to="#">لینک چهارم منو</Link>
       </div>
+      <button
+        className={styles.toggleButton}
+        onClick={() => setIsMenuClosed(isMenuClosed => !isMenuClosed)}
+      >
+        {isMenuClosed ? "open" : "close"}
+      </button>
     </nav>
   );
 };
 
-export default NavBar;
+export default withRouter(NavBar);
