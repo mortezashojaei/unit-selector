711aae009a1acec23e4ff278e2fb89b042e393a1
sinamoraddar <sinamoraddar@gmail.com>
2020-01-08 20:27:12 +0330

    finalize the styles for the navbar

--- src/Components/Dashboard/NavBar/NavBar.js
+++ src/Components/Dashboard/NavBar/NavBar.js
@@ -1,6 +1,8 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useState, useEffect } from "react";
 import { Link, Redirect, withRouter } from "react-router-dom";
+import { CSSTransitionGroup } from "react-transition-group";
 import styles from "./NavBar.module.scss";
+import "./NavBarAnimations.scss";
 
 const NavBar = ({ history }) => {
   const [isMenuClosed, setIsMenuClosed] = useState(true);
@@ -9,13 +11,12 @@ const NavBar = ({ history }) => {
     history.push("/");
   }, [history]);
 
-  const toggleMenu = useCallback(
-    () => setIsMenuClosed(isMenuClosed => !isMenuClosed),
-    []
-  );
+  const toggleMenu = useCallback(() => {
+    setIsMenuClosed(isMenuClosed => !isMenuClosed);
+  }, []);
 
   return (
-    <nav className={styles.navBar}>
+    <nav className={`${styles.navBar} `}>
       <div className={styles.logo}>لوگو</div>
       <div className={styles.buttonContainer}>
         <button onClick={logout}>
@@ -25,11 +26,25 @@ const NavBar = ({ history }) => {
           ویرایش اطلاعات <span className={styles.circle}></span>
         </button>
       </div>
-      <div
-        className={`${styles.linkContainer} ${
-          isMenuClosed ? styles.closed : null
-        }`}
+      <CSSTransitionGroup
+        transitionName="navBar"
+        transitionEnterTimeout={600}
+        transitionLeave={true}
+        transitionLeaveTimeout={600}
+        transitionAppearTimeout={600}
+        transitionAppear={true}
       >
+        {!isMenuClosed && (
+          <div className={styles.linkContainer}>
+            <Link to="#">لینک اول منو</Link>
+            <Link to="#">لینک دوم منو</Link>
+            <Link to="#">لینک سوم منو</Link>
+            <Link to="#">لینک چهارم منو</Link>
+          </div>
+        )}
+      </CSSTransitionGroup>
+      {/* show this one on larger screens only */}
+      <div className={`${styles.linkContainer} ${styles.largeScreens}`}>
         <Link to="#">لینک اول منو</Link>
         <Link to="#">لینک دوم منو</Link>
         <Link to="#">لینک سوم منو</Link>
