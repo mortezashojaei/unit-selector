f1e40799aeeee4c40068ee40f00a0df9a424a29a
sajjad77 <sajjadmashhadi7@gmail.com>
2020-01-09 12:27:05 +0330

    add more burron for courses list and customize scroll bar

--- src/Components/Dashboard/CourseList/CourseList.js
+++ src/Components/Dashboard/CourseList/CourseList.js
@@ -3,12 +3,20 @@ import CourseListItem from '../CourseListItem/CourseListItem'
 import styles from './CourseList.module.scss'
 
 const CourseList = props => {
+    const [showMore, setShowMore] = useState(false)
+    const onMoreClick = () => {
+        setShowMore(!showMore)
+    }
     return (
         <div className={styles.mainDiv}>
-            <div className={styles.list}>
+            <div className={`${styles.list} ${showMore && styles.scroll}`}>
                 {props.courses.map(course => <CourseListItem key={course.id} onSelect={props.onSelect} {...course}/>)}
             </div>
-            <button>بیش تر...</button>
+            <button 
+            onClick={onMoreClick} 
+            className={`${styles.more} ${showMore && styles.hide}`}>
+            {!showMore && <span>بیش تر...</span>}
+            </button>
         </div>
     )
 }
