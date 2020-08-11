b2e240c24448ebe72286b5dfacb6f6d81b9b5e01
morteza shojaei <mshojaei179@gmail.com>
2020-08-11 22:58:14 +0430

    complete api calls tests

--- src/Utils/__tests__/ApiCalls.test.js
+++ src/Utils/__tests__/ApiCalls.test.js
@@ -1,18 +1,17 @@
 const { fetchMajors } = require("Utils/ApiCalls/FetchList");
 const { doesEmailExist } = require("Utils/ApiCalls/Auth");
+const { fetchCourses } = require("Utils/ApiCalls/CourseBox");
 
-test("majors fetches successfully", () => {
-  fetchMajors().then((response) => expect(response.StatusCode).toEqual(200));
-});
+test("majors fetches successfully", async (done) => {
+  const response = await fetchMajors();
+  expect(response.status).toEqual(200);
+  done();
+}, 15000);
 
-test("uniq mails checks successfully", () => {
-  doesEmailExist({ email: "I_AM_UNIQ_EMAIL_ADDRESS@b.com" }).then((response) =>
-    expect(response.StatusCode).toEqual(200)
-  );
-});
-
-test("ununiq mails checks successfully", () => {
-  doesEmailExist({ email: "a@f.com" }).then((response) =>
-    expect(response.StatusCode).toEqual(200)
-  );
-});
+test("uniq mails checks successfully", async (done) => {
+  const response = await doesEmailExist({
+    email: "I_AM_UNIQ_EMAIL_ADDRESS@b.com",
+  });
+  expect(response.status).toEqual(200);
+  done();
+}, 15000);
